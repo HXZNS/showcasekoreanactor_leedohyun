16b854b701f698a0937259423468aa1b1fd6d5d5
HXZNS <phonchanoksuriwong54@gmail.com>
2026-03-16 12:08:32 +0700

    first commit

---
+++ JavaScript/script4.js
@@ -0,0 +1,24 @@
+let container = document.querySelector('.container');
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+container.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.heading',{delay:200, origin:'top'});
+sr.reveal('.dream',{delay:450, origin:'top'});
+sr.reveal('.btn',{delay:450, origin:'bottom'});
